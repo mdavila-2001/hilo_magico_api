26edd58693686ddf4787a8ce9a607d5f98aa1a45
Marcelo Dávila <dmarcelo201@gmail.com>
2025-05-19 19:09:24 -0400

    clear data

--- app/models/user.py
+++ app/models/user.py
@@ -1,27 +1,28 @@
-from sqlalchemy import Column, String, Boolean, Integer, DateTime
-from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy.orm import relationship
 import uuid
 from datetime import datetime
+from sqlalchemy import Column, String, Boolean, DateTime, Integer
+from sqlalchemy.dialects.postgresql import UUID
 from app.db.session import Base
-from app.models.user_store import user_store
 
 class User(Base):
     __tablename__ = "users"
     __table_args__ = {"schema": "public"}
-    
+
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
-    email = Column(String, unique=True, index=True, nullable=False)
-    hashed_password = Column(String, nullable=False)
+    email = Column(String, unique=True, nullable=False, index=True)
+    ci = Column(String, unique=True, nullable=False, index=True)
+
     first_name = Column(String, nullable=False)
     middle_name = Column(String, nullable=True)
     last_name = Column(String, nullable=False)
     mother_last_name = Column(String, nullable=True)
-    is_active = Column(Boolean, default=True)
-    role = Column(Integer, default=3)  # 1: admin, 2: emprendedor, 3: cliente
+
+    hashed_password = Column(String, nullable=False)
+    phone = Column(String, nullable=True)
+
+    role_id = Column(Integer, default=3)
+    status = Column(Boolean, default=True)
+
     created_at = Column(DateTime, default=datetime.utcnow)
     updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
     deleted_at = Column(DateTime, nullable=True)
-    
-    # Relación many-to-many con Store
-    stores = relationship("Store", secondary=user_store, back_populates="users")
